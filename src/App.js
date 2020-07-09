3776ce4e0360b129ad41272c9cafcac587684f0d
Pramish Luitel <pramish.luitel7@gmail.com>
2020-07-10 02:42:47 +1000

    auth page designed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
-
 function App() {
+  const [authPage, setAuthPage] = React.useState(true);
+  const [register, setRegister] = React.useState(true);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className='navbar-fixed'>
+        <nav>
+          <div className='nav-wrapper menu'>
+            <a href='#' className='brand-logo'>
+              Greetings
+            </a>
+            <ul id='nav-mobile' className='right hide-on-med-and-down'>
+              <li>
+                <button
+                  onClick={() => setAuthPage(() => !register)}
+                  data-target='modal1'
+                  className='account btn '
+                  type='submit'
+                  name='action'
+                >
+                  My Account
+                </button>
+                {authPage}
+              </li>
+            </ul>
+          </div>
+        </nav>
+      </div>
+      {authPage ? (
+        <>
+          <p className='logintext'>
+            {register ? 'Create Your Account' : 'Access your account'}
+          </p>
+          <form>
+            <div className='input-field col s6'>
+              <input id='email' type='text' className='validate' />
+              <label for='email'>Email Address</label>
+            </div>
+            <div className='input-field col s6'>
+              <input id='password' type='password' className='validate' />
+              <label for='password'>Password</label>
+            </div>
+            {register ? (
+              <>
+                <div className='input-field col s6'>
+                  <input id='cpassword' type='password' className='validate' />
+                  <label for='cpassword'>Confirm Password</label>
+                </div>
+                <div class='input-field col s6'>
+                  <input id='name' type='text' className='validate' />
+                  <label for='name'>Name</label>
+                </div>
+                <div className='input-field col s6'>
+                  <input type='text' id='date' className='validate' />
+                  <label for='date'>Date of Birth</label>
+                </div>
+              </>
+            ) : (
+              <></>
+            )}
+
+            <div className='submitbtn'>
+              <button
+                class='btn waves-effect waves-light'
+                type='submit'
+                name='action'
+              >
+                {register ? 'Register' : 'Login'}
+              </button>
+              <div>
+                <p
+                  onClick={() => setRegister((register) => !register)}
+                  className='logintext'
+                >
+                  {register
+                    ? 'Already have an account?'
+                    : "Don't Have account?"}
+                </p>
+              </div>
+            </div>
+          </form>
+        </>
+      ) : (
+        <></>
+      )}
     </div>
   );
 }
