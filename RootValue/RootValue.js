41b8212e920a49ce9f4c6131338d12aafc4a3aba
Pramish Luitel <pramish.luitel7@gmail.com>
2020-08-17 21:02:00 +1000

    removed the authentication from add friends

--- RootValue/RootValue.js
+++ RootValue/RootValue.js
@@ -93,10 +93,10 @@ module.exports = RootValue = {
   },
   addFriends: async (args, req) => {
     try {
-      const isAuth = await req.isAuth;
-      if (!isAuth) throw new Error('Please autheticate');
-      const userId = await req.userId;
-
+//       const isAuth = await req.isAuth;
+//       if (!isAuth) throw new Error('Please autheticate');
+//       const userId = await req.userId;
+      const userId = "5f39241f71c02700173945e5";
       const name = args.friendsInput.name;
       const email = args.friendsInput.email;
       const date_of_birth = args.friendsInput.date_of_birth;
